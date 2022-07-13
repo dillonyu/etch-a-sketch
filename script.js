dba1dfa279826c88476582d9007796e44abb21bd
Dillon Yu <86173315+dillonyu@users.noreply.github.com>
2022-07-13 14:30:36 -0400

    Add custom dimensions button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function createGrid(num) {
     const grid = document.querySelector('#grid');
+    grid.replaceChildren();
     let squareDimension = getComputedStyle(grid).maxWidth.slice(0, -2)/num;
     for (let i = 0; i < num * num; i++) {
         const square = document.createElement('div');
@@ -11,5 +12,15 @@ function createGrid(num) {
         grid.appendChild(square);
     }
 }
+
+const dimensionButton = document.querySelector("#dimension");
+dimensionButton.addEventListener('click', () => {
+    let input = prompt("How many squares per side? (max 100)");
+    while ((input > 100 || input < 1 || isNaN(input)) && input !== null) {
+        alert("Please enter a number between 1 and 100.");
+        input = prompt("How many squares per side? (max 100)");
+    }
+    createGrid(input);
+});
 
 createGrid(10);
