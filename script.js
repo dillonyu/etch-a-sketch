1721cde20269c4be74065f0591ba58945fe858df
Dillon Yu <86173315+dillonyu@users.noreply.github.com>
2022-07-13 13:21:33 -0400

    Add createGrid function and double grid size

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const grid = document.querySelector('#grid');
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+function createGrid(num) {
+    const grid = document.querySelector('#grid');
+    let squareDimension = getComputedStyle(grid).maxWidth.slice(0, -2)/num;
+    console.log(squareDimension);
+    for (let i = 0; i < num * num; i++) {
         const square = document.createElement('div');
-        square.style.padding = '10px';
+        square.style.width = `${squareDimension}px`;
+        square.style.height = `${squareDimension}px`;
         square.addEventListener('mouseover', (e) => {
             e.target.style.background = 'DeepSkyBlue';
         })
         grid.appendChild(square);
     }
 }
+
+createGrid(10);
