874843af4bfd7801042b05d1d48668ce2fb0b941
Dillon Yu <86173315+dillonyu@users.noreply.github.com>
2022-07-13 12:46:09 -0400

    Reverse border syntax

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement('div');
         square.style.padding = '10px';
-        square.style.border = 'solid black 1px';
+        square.style.border = '1px solid black';
         grid.appendChild(square);
     }
 }
